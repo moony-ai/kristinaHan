ea303f291e7cbb88ddef98808247e23da70f5954
짱짱맨 <60203153+kksjjm@users.noreply.github.com>
2024-01-15 16:00:10 +0000

    init

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import OrderForm from './OrderForm.js'; // OrderForm 컴포넌트의 경로를 정확히 지정해야 합니다.
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/order-form" element={<OrderForm />} />
+        {/* 여기에 추가적인 라우트들을 정의할 수 있습니다 */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
