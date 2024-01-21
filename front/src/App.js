595c71d52982e3a25c322c8ec0e0656a3c98295d
짱짱맨 <60203153+kksjjm@users.noreply.github.com>
2024-01-21 09:32:27 +0000

    hash-router/front

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import OrderList from './OrderList';
 import OrderForm from './OrderForm';
 import OrderFormLoaded from './OrderFormLoaded';
