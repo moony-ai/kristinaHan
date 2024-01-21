541ab74e3d6f2cf5d59d4549c70b964660b38f6f
짱짱맨 <60203153+kksjjm@users.noreply.github.com>
2024-01-21 09:22:51 +0000

    url update

--- server/urls.py
+++ server/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/orders/', include('orders.urls')),
-    path('sentry-debug', views.trigger_error),
+    path('sentry-debug/', views.trigger_error),
 ]
