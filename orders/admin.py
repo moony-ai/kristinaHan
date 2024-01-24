0f413b116b9c7c8abdf6b1fb09fc76ea9c696595
짱짱맨 <60203153+kksjjm@users.noreply.github.com>
2024-01-24 06:47:31 +0000

    서버 admin 삭제 기능 추가

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Order
 
-admin.site.register(Order)
+def delete_selected_orders(modeladmin, request, queryset):
+    queryset.delete()
+
+delete_selected_orders.short_description = '선택한 주문들을 삭제'
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['ordererName', 'affiliation', 'contact', ...]  # 원하는 필드 나열
+    actions = [delete_selected_orders]
+
+admin.site.register(Order, OrderAdmin)
