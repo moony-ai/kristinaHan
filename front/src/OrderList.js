49aba5a6d1fe49204bd25ac412be5786a57a1eed
handevmin <handevmin@gmail.com>
2024-03-11 10:26:29 +0900

    월별 주문 내역 보기 기능 추가

--- front/src/OrderList.js
+++ front/src/OrderList.js
@@ -12,6 +12,8 @@ class OrderList extends Component {
       deletedOrders: [],  // 삭제된 주문 목록을 저장할 상태 추가
       error: null,
       activeTab: 'orders', // 현재 활성화된 탭을 추적 ('orders', 'newOrders', 'deletedOrders')
+      selectedYear: new Date().getFullYear(), // 현재 연도를 기본값으로 설정
+      selectedMonth: new Date().getMonth() + 1, // 현재 월을 기본값으로 설정
     };
   }
 
@@ -24,13 +26,32 @@ class OrderList extends Component {
 
   // 기존목록
   fetchOrders() {
-    axios.get('https://server-6kol.onrender.com/api/v1/orders/')
+    axios.get('http://127.0.0.1:8000/api/v1/orders/')
       .then(response => {
         this.setState({ orders: response.data });
       })
       .catch(error => {
         this.setState({ error });
       });
+  }
+
+  // 월별 주문 목록
+  fetchMonthlyOrders = (year, month) => {
+    axios.get(`http://127.0.0.1:8000/api/v1/orders/${year}/${month}/`)
+      .then(response => {
+        this.setState({ orders: response.data });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  }  
+
+  handleYearChange = (event) => {
+    this.setState({ selectedYear: event.target.value });
+  }
+
+  handleMonthChange = (event) => {
+    this.setState({ selectedMonth: event.target.value });
   }
 
   // 신규목록
@@ -114,49 +135,95 @@ class OrderList extends Component {
 
 
   render() {
-    const { orders, newOrders, marriedOrders, deletedOrders, error, activeTab } = this.state;
+    const { orders, newOrders, marriedOrders, deletedOrders, error, activeTab, selectedYear, selectedMonth} = this.state;
 
     if (error) {
       return <div>오류가 발생했습니다: {error.message}</div>;
     }
 
-    return (
-      <div>
-        <section style={{ display: "flex", justifyContent: "space-around" }}>
-          <h2><Link to={'/orders/new'}>미혼자용 주문작성 (신규)</Link></h2>
-          <h2><Link to={'/orders/married'}>기혼자용 주문작성 (2025입궁식)</Link></h2>
+    return  (
+      <div className="container-fluid mt-5 px-0">
+      <section className="d-flex justify-content-around mb-4 custom-header">
+          <h2 style={{ fontWeight: 'bold' }}><Link to={'/orders/new'}>미혼자용 주문작성 (신규)</Link></h2>
+          <h2 style={{ fontWeight: 'bold' }}><Link to={'/orders/married'}>기혼자용 주문작성 (2025입궁식)</Link></h2>
         </section>
-        <h2>주문 목록</h2>
-        <button style={{ marginLeft:'20px'}} onClick={this.handleDownloadExcel}>엑셀로 다운로드</button>
-        <button style={{ marginLeft:'20px'}} onClick={this.handleUpdateSpreadsheet}>스프레드시트 업데이트</button>
-        <section style={{ padding:'20px'}}>
-          <button style={{ marginLeft:'20px'}} onClick={() => this.setState({ activeTab: 'orders' })}>기존 주문서</button>
-          <button style={{ marginLeft:'20px'}} onClick={() => this.setState({ activeTab: 'newOrders' })}>신규 주문서(2024 3월 이후)</button>
+
+
+        <div className="text-left mb-4">
+        <button className="custom-button" style={{ marginLeft:'20px'}} onClick={this.handleDownloadExcel}>엑셀로 다운로드</button>
+        <button className="custom-button" style={{ marginLeft:'20px'}} onClick={this.handleUpdateSpreadsheet}>스프레드시트 업데이트</button>
+        </div>
+      <section className="text-left mb-4">
+          <button className="custom-button" style={{ marginLeft:'20px',  marginBottom: '20px'}} onClick={() => this.setState({ activeTab: 'orders' })}>기존 주문서</button>
+          <button className="custom-button" style={{ marginLeft:'20px',  marginBottom: '20px'}} onClick={() => this.setState({ activeTab: 'newOrders' })}>신규 주문서(2024 3월 이후)</button>
           {/* <button style={{ marginLeft:'20px'}} onClick={() => this.setState({ activeTab: 'marriedOrders' })}>기혼자용 주문서</button> */}
-          <button style={{ marginLeft:'20px'}} onClick={() => this.setState({ activeTab: 'deletedOrders' })}>휴지통</button>
+          <button className="custom-button" style={{ marginLeft:'20px',  marginBottom: '20px'}} onClick={() => this.setState({ activeTab: 'deletedOrders' })}>휴지통</button>
         </section>
+
+
+        <div className="row mb-1">
+          <div className="col d-flex align-items-center">
+            <h2 className="mb-4" style={{  marginLeft:'20px', fontWeight: 'bold' }}>주문 목록</h2>
+          </div>
+          <div className="col-auto d-flex justify-content-center" style={{ marginRight: '-10px' }}>
+            <select className="form-control" value={selectedYear} onChange={this.handleYearChange}>
+              {[...Array(5).keys()].map(offset => {
+                const year = new Date().getFullYear() - 2 + offset; // 현재 연도 기준으로 -2년부터 +2년까지 선택 가능
+                return <option key={year} value={year}>{year}</option>;
+              })}
+            </select>
+          </div>
+          <div className="col-auto d-flex justify-content-center" style={{ marginRight: '-10px' }}>
+            <select className="form-control" value={selectedMonth} onChange={this.handleMonthChange}>
+              {[...Array(12).keys()].map(month => (
+                <option key={month + 1} value={month + 1}>{month + 1}월</option>
+              ))}
+            </select>
+          </div>
+          <div className="col-auto justify-content-center">
+            <button className="custom-button" style={{ marginRight:'20px' }} onClick={() => this.fetchMonthlyOrders(this.state.selectedYear, this.state.selectedMonth)}>월별 주문 조회</button>
+          </div>
+        </div>
+
+
         {activeTab === 'orders' && (
           // 기존 주문서 리스트 렌더링
-          <ul>
+          <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>주문 번호</th>
+              <th>주문자 이름</th>
+              <th>소속</th>
+              <th>연락처</th>
+              <th>주문 상태</th>
+              <th>생성자</th>
+              <th>생성 시간</th>
+              <th>총액</th>
+              <th>잔액</th>
+              <th>조치</th>
+            </tr>
+          </thead>
+          <tbody>
             {orders.map(order => (
-              <li key={order.orderNumber} style={{ display: 'flex', marginBottom: '10px' }}>
-                <div style={{ marginRight: '20px' }}>
-                  <Link to={`/orders/${order.id}`}>주문 번호: {order.orderNumber}</Link>
-                </div>
-                <div style={{ marginRight: '20px' }}>주문자 이름: {order.ordererName}</div>
-                <div style={{ marginRight: '20px' }}>소속: {order.affiliation}</div>
-                <div style={{ marginRight: '20px' }}>연락처: {order.contact}</div>
-                <div style={{ marginRight: '20px' }}>주문 상태: {order.orderStatus}</div>
-                <div style={{ marginRight: '20px' }}>생성자: {order.creator}</div>
-                <div style={{ marginRight: '20px' }}>생성 시간: {order.creationTime}</div>
-                <div style={{ marginRight: '20px' }}>총액: {order.totalAmount}</div>
-                <div>잔액: {order.balance}</div>
-                <button onClick={() => this.deleteOrder(order.id)} style={{ marginLeft: '20px' }}>
-                  삭제
-                </button>
-              </li>
+              <tr key={order.orderNumber}>
+                <td><Link to={`/orders/${order.id}`}>{order.orderNumber}</Link></td>
+                <td>{order.ordererName}</td>
+                <td>{order.affiliation}</td>
+                <td>{order.contact}</td>
+                <td>{order.orderStatus}</td>
+                <td>{order.creator}</td>
+                <td>{new Date(order.creationTime).toLocaleString()}</td>
+                <td>{order.totalAmount.toLocaleString()}</td>
+                <td>{order.balance.toLocaleString()}</td>
+                <td>
+                  <button className="btn btn-danger btn-sm" onClick={() => this.deleteOrder(order.id)}>
+                    삭제
+                  </button>
+                </td>
+              </tr>
             ))}
-          </ul>
+          </tbody>
+        </table>
         )}
         {activeTab === 'newOrders' && (
           // 신규 주문서 리스트 렌더링
